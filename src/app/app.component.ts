856162c7a02fbbe114e10c43f077d044bf5cf6d5
Saumitra Kahate <saumitra.kahate@zensar.com>
2020-01-28 18:58:27 +0530

    Resolved Conflicts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-<<<<<<< HEAD
   //styleUrls: ['./app.component.css']
   styles:[`
     h3 {color:green;}
   `]
-=======
-  styleUrls: ['./app.component.css']
->>>>>>> 521255ab8ea626981c3b344321b34d0e8e6f1fca
 })
 export class AppComponent {
  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
-<<<<<<< HEAD
 import { ServersComponent } from './servers/servers/servers.component';
-=======
->>>>>>> 521255ab8ea626981c3b344321b34d0e8e6f1fca
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     ServerComponent,
     ServersComponent
-=======
-    ServerComponent
->>>>>>> 521255ab8ea626981c3b344321b34d0e8e6f1fca
   ],
   imports: [
     BrowserModule,
